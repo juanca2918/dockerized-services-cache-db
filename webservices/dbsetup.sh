d48bf2e93aab30635204d397e1cb1e61ed3041da
juanca2918 <jacal2211@gmail.com>
2024-01-10 12:27:46 -0500

    Se cambia contraseña generica de db

--- webservices/dbsetup.sh
+++ webservices/dbsetup.sh
@@ -6,6 +6,6 @@ service postgresql start
 
 su - postgres -c "createdb -E UTF8 -T template0 --locale=en_US.utf8 -O sisconced" 
 
-CREATE USER desarrollador WITH PASSWORD 'sisconced';
+CREATE USER desarrollador WITH PASSWORD 'mysecretpassword';
 
 GRANT ALL PRIVILEGES ON DATABASE sisconced TO desarrollador;
