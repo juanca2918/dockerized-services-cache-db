160bffbe0bc11377c0e3f4f4fd4581f6366c8fd0
juanca2918 <jacal2211@gmail.com>
2024-01-11 18:19:30 -0500

    Inicio de Testing

--- webservices/app/appsetup.sh
+++ webservices/app/appsetup.sh
@@ -1,8 +1,22 @@
 # bash appsetup.sh
 
-chmod +x appsetup.sh
+#!/bin/bash
 
-cd /var/www/sisconced/
+# Iniciar el servicio
+php-fpm &
+
+# Esperar a que se cree el archivo de socket
+while [ ! -e /var/run/php/php8.2-fpm.sock ]; do sleep 1; done
+
+# Cambiar el propietario del archivo de socket
+chown www-data:www-data /var/run/php/php8.2-fpm.sock
+
+ls -l /var/run/php/php8.2-fpm.sock
+
+# Mantener el contenedor en ejecución
+tail -f /dev/null
+
+chmod +x appsetup.sh
 
 composer i
 
@@ -11,5 +25,7 @@ php artisan key:generate
 npm i
 
 npm run dev
+
+php artisan migrate
 
 php artisan serve --host=127.0.0.1 --port=80
